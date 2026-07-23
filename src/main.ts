6479e41d311b4f43a4231e6da7591519b2299716
dajhiro <kuz.edu98@gmail.com>
2026-07-23 15:03:33 +0900

    Feat: Add ribbon icon to open homepage

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Plugin } from 'obsidian';
 import { DEFAULT_SETTINGS, SimpleHomepageSettings, SimpleHomepageSettingTab } from './settings';
-import { openHomepage } from './homepage';
+import { openHomepage, OpenMode } from './homepage';
 
 export const APP_NAME = "Simple Homepage";
 
@@ -10,6 +10,30 @@ export default class SimpleHomepage extends Plugin {
 	async onload() {
     await this.loadSettings();
     this.addSettingTab(new SimpleHomepageSettingTab(this.app, this));
+
+    // Ribbon Icon: Open Homepage
+    this.addRibbonIcon(
+      'home',
+      "Open homepage",
+      (evt: MouseEvent) => {
+        const { shiftKey, ctrlKey, metaKey, altKey } = evt;
+        const isCtrl = ctrlKey || metaKey;
+
+        let mode: OpenMode;
+
+        if (isCtrl) {
+          mode = 'tab';
+        } else if (shiftKey) {
+          mode = 'window';
+        } else if (altKey) {
+          mode = 'split';
+        } else {
+          mode = 'current';
+        }
+
+        void openHomepage(this.app, this.settings.homepagePath, mode);
+      }
+    );
 
     // Command: Simple Homepage: Open Homepage
     this.addCommand({
